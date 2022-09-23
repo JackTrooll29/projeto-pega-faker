1ea4045687a5f2e3ed072a9f681760c40f6ca3d6
JackTrooll29 <powerjack825@gmail.com>
2022-09-22 21:46:12 -0300

    02

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-import requests
+# import requests
 
 app = FastAPI()
 
 
 @app.get("/")
 def my_ip():
-    ip_publico = requests.get('https://api.ipify.org/').text
-    return {"IP Publico": ip_publico}
+    # ip_publico = requests.get('https://api.ipify.org/').text
+    return {"IP Publico": 'ip_publico'}
     
