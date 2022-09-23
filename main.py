675e45a57591c596c55e7dff85ae21905589c651
JackTrooll29 <powerjack825@gmail.com>
2022-09-22 22:21:39 -0300

    04

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-# import requests
+import requests
+import socket
 
 app = FastAPI()
 
 
 @app.get("/")
 def my_ip():
-    # ip_publico = requests.get('https://api.ipify.org/').text
-    return {"IP Publico": 'ip_publico'}
+    ip_local = socket.gethostbyname(socket.gethostname())
+    ip_publico = requests.get('https://api.ipify.org/').text
+    return {"IP Publico": ip_publico, "IP Local": ip_local}
     
